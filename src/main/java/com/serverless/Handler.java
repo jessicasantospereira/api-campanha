310295ab7f40675df1304f284c4a78578369321c
jessicasantospereira <jessydfj@gmail.com>
2023-04-06 17:00:47 -0300

    atualização código

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,7 +1,11 @@
 package com.serverless;
 
+import java.net.HttpURLConnection;
 import java.util.Map;
 
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.google.gson.Gson;
 import com.serverless.core.dto.CodigoPromocionalDto;
 import com.serverless.core.entidade.ApiGatewayResponse;
 import com.serverless.outbound.CodigoPromocionalRepository;
@@ -11,26 +15,30 @@ import org.apache.logging.log4j.Logger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class Handler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
 
 	private static final Logger LOG = LogManager.getLogger(Handler.class);
 
+
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+	public AwsProxyResponse handleRequest(AwsProxyRequest request, Context context) {
+		AwsProxyResponse response = new AwsProxyResponse((HttpURLConnection.HTTP_OK));
+		LOG.info("Bin recebido para consulta {} " , request.getPathParameters().get("valor"));
+
+		String bin = request.getPathParameters().get("valor");
 
 		CodigoPromocionalRepository repository = new CodigoPromocionalRepository();
-		CodigoPromocionalDto codigoPromocional = repository.findByBin(input.get("bin").toString());
+		CodigoPromocionalDto codigoPromocional = repository.findByBin(bin);
 
 		if (codigoPromocional == null) {
 			LOG.info("código não localizado");
-			return ApiGatewayResponse.builder()
-					.setStatusCode(404)
-					.build();
+			response = new AwsProxyResponse((HttpURLConnection.HTTP_BAD_REQUEST));
+			response.setBody("código não localizado");
+			return response;
 		}
-		LOG.info("Código encontrado: {}", codigoPromocional);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(codigoPromocional)
-				.build();
+		LOG.info("Código encontrado: {}", codigoPromocional.toString());
+
+		response.setBody(codigoPromocional.toString());
+		return response;
 	}
 }
